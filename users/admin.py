3b0585d0179339ec60fa8c7e578a2c81d54f8ee3
Abhinav Chavali <abhinavchavali12@gmail.com>
2020-12-05 21:24:28 -0500

    added Custom User Model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+CustomUser = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'age']
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+
+
--- users/models.py
+++ users/models.py
@@ -4,16 +4,18 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, age=None):
         if not email:
             raise ValueError('Please set the email')
         email = self.normalize_email(email)
-        user = self.model(email=email.lower(), age=extra_fields.get('age', 0))
+        user = self.model(email=email.lower(), age=age)
         user.set_password(password)
         user.save()
 
-    def create_superuser(self, email, password, **extra_fields):
-        user = self.create_user(email, password, extra_fields)
+        return user
+
+    def create_superuser(self, email, password, age):
+        user = self.create_user(email, password, age=age)
         user.is_staff = True
         user.is_superuser = True
         user.save()
@@ -25,9 +27,9 @@ class CustomUser(AbstractUser):
     created = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['age']
 
-    objects  = CustomUserManager()
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
