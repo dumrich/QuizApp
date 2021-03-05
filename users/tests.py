1c19524b2a9468073a2b08af4f92754ecde19b4d
Abhinav Chavali <abhinavchavali12@gmail.com>
2021-03-05 16:36:28 -0500

    added first tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    '''Test Custom User Model'''
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="test@gmail.com",
+            age=14,
+            password="testpass123"
+        )
+        self.assertEqual(user.email, "test@gmail.com")
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        '''Test creation of superuser'''
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            email="test@gmail.com",
+            age=14,
+            password="testpass123"
+        )
+        self.assertEqual(user.email, "test@gmail.com")
+        self.assertTrue(user.is_superuser)
